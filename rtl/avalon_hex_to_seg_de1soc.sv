56735f5534fab0c69d4cd58bd4b96ff887b67451
Ivan Sotnikov <stn_iv@list.ru>
2019-01-27 14:44:58 +0300

    Edited for compabilety with Qsys in quartus 16.1

--- rtl/avalon_hex_to_seg_de1soc.sv
+++ rtl/avalon_hex_to_seg_de1soc.sv
@@ -15,23 +15,23 @@ module avalon_hex_to_seg_de1soc #(
     logic [NUM_SEGMENT - 1:0][3:0][7:0] data_to_out;
     logic [NUM_SEGMENT - 1:0][6:0] segment_symbol_s;
     assign avms_writedata_s = avms_writedata_i;
-
+    integer word_ind,byte_ind ;
     always_ff @(posedge clk) begin
         if( !rst_n ) begin
            data_to_out <= '0;
         end else if ( avms_write_i ) begin
-            for (int word_ind = 0; word_ind < NUM_SEGMENT; word_ind++) begin
+            for ( word_ind = 0; word_ind < NUM_SEGMENT; word_ind++) begin
                 if(avms_address_i == word_ind) begin
-                    for (int byte_ind = 0; byte_ind < 4; byte_ind++) begin
+                    for ( byte_ind = 0; byte_ind < 4; byte_ind++) begin
                         data_to_out[word_ind][byte_ind] <= avms_byteenable_i[byte_ind] ? avms_writedata_s[byte_ind] : data_to_out[word_ind][byte_ind];
                     end
                 end
             end
         end
     end
-
+    genvar seg_ind ;
     generate
-        for (genvar seg_ind = 0; seg_ind < NUM_SEGMENT; seg_ind++) begin
+        for ( seg_ind = 0; seg_ind < NUM_SEGMENT; seg_ind++) begin : segment_conver_genblck
                 hex_to_segment_convert_de1soc i_hex_to_segment_convert_de1soc (
                     .clk_i ( clk ) ,
                     .rst_n_i ( rst_n ) ,
